7276307c573716e85ed97982ebbf1f7ae8807303
John Doe <johndoe@example.com>
2024-05-18 08:21:29 +0300

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+1. add addresses in `addresses.txt`
+1. run `npm run start`
+1. check sybil addresses into `result.txt` 
---
+++ index.cjs
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+const newLine = /\r?\n/;
+const encoding = "utf-8";
+
+const sybils = fs
+  .readFileSync("sybil-addresses.txt", { encoding })
+  .split(newLine);
+
+fs.writeFileSync(
+  "result.txt",
+  fs
+    .readFileSync("addresses.txt", { encoding })
+    .split(newLine)
+    .map((a) => (sybils.includes(a.toLowerCase()) ? `${a},sybil` : `${a}`))
+    .join("\n"),
+  { encoding }
+);
