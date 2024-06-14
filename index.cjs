da89cf9f4184864dff8f950a86edd9e09e9d8ab8
John Doe <johndoe@example.com>
2024-06-15 00:36:07 +0300

    prepare to final list

--- index.cjs
+++ index.cjs
@@ -1,23 +1,21 @@
 const fs = require("fs");
 
-const FILE_SYBILS = "trusta-addresses.txt";
-
 const newLine = /\r?\n/;
 const encoding = "utf-8";
 
 const sybils = fs
-  .readFileSync(FILE_SYBILS, { encoding })
+  .readFileSync("sybils.txt", { encoding })
   .split(newLine)
   .map((l) => l.toLocaleLowerCase());
 
 const data = fs
-  .readFileSync("my-addresses.txt", { encoding })
+  .readFileSync("addresses.txt", { encoding })
   .split(newLine)
-  .map((a) => (sybils.includes(a.toLowerCase()) ? `${a},sybil` : `${a},`));
+  .map((l) => l.toLocaleLowerCase())
+  .map((a) => (sybils.includes(a) ? `${a},sybil` : a));
 
-fs.writeFileSync("result-all.txt", data.join("\n"), { encoding });
-fs.writeFileSync(
-  "result-sybils.txt",
-  data.filter((l) => l.endsWith("sybil")).join("\n"),
-  { encoding }
-);
+fs.writeFileSync("result.txt", data.join("\n"), { encoding });
+
+let count = data.filter((l) => l.endsWith("sybil")).length;
+
+console.log(`sybils: ${count}`);
