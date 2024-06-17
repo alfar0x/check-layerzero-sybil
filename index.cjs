6712161729442f544d3ae731b6af09e4ce423b48
John Doe <johndoe@example.com>
2024-06-17 17:39:46 +0300

    progress log

--- index.cjs
+++ index.cjs
@@ -2,33 +2,58 @@ const fs = require("fs");
 
 const encoding = "utf-8";
 
-const readFile = (name) => fs.readFileSync(name, { encoding }).split(/\r?\n/);
+const read = (name) => fs.readFileSync(name, { encoding }).split(/\r?\n/);
+const append = (name, line) =>
+  fs.appendFileSync(name, `${line}\n`, { encoding });
+const write = (name, data) => fs.writeFileSync(name, data, { encoding });
+
 const normalizeAddress = (a) => a.trim().toLocaleLowerCase();
 
-const addresses = readFile("addresses.txt").map(normalizeAddress);
-const sybils = readFile("provisionalSybilList.csv");
+write("result.txt", "");
+write("sybils.txt", "");
 
-const found = {};
+const addresses = read("addresses.txt").map(normalizeAddress);
+const sybils = read("provisionalSybilList.csv").map((line) => {
+  const splitted = line.split(",");
+  if (splitted.length !== 3) return { url: "", cluster: "", address: "" };
+  return {
+    url: splitted[0],
+    cluster: splitted[1],
+    address: normalizeAddress(splitted[2]),
+  };
+});
 
-for (const sybil of sybils) {
-  const splitted = sybil.split(",");
-  if (splitted.length !== 3) continue;
-  const lowerAddress = normalizeAddress(splitted[2]);
-  if (!addresses.includes(lowerAddress)) continue;
-  if (!found[lowerAddress]) found[lowerAddress] = [];
-  found[lowerAddress].push([splitted[0], splitted[1]].join(" , "));
+let idx = 0;
+let nextLog = 10;
+let sybilsCount = 0;
+
+for (const address of addresses) {
+  const reports = [];
+
+  for (const sybil of sybils) {
+    if (sybil.address === address) {
+      reports.push(`${sybil.url} , ${sybil.cluster}`);
+    }
+  }
+
+  if (reports.length) {
+    append("result.txt", `${address} - ${reports.join(" | ")}`);
+    append("sybils.txt", `${address}\n\n${reports.join("\n")}\n\n---\n`);
+
+    sybilsCount += 1;
+  } else {
+    append("result.txt", `${address},not sybil`);
+  }
+
+  idx += 1;
+
+  const progress = (idx / addresses.length) * 100;
+  if (progress >= nextLog) {
+    console.log(`${nextLog}%`);
+    nextLog += 10;
+  }
 }
 
-const result = addresses
-  .map((a) => (found[a] ? [a, found[a].join(" | ")].join(" - ") : a))
-  .join("\n");
+if (!sybilsCount) write("sybils.txt", "no sybils");
 
-fs.writeFileSync("result.txt", result, { encoding });
-
-const sybilsResult = Object.keys(found)
-  .map((a) => `${a}\n${found[a].join("\n")}`)
-  .join("\n\n----\n\n");
-
-fs.writeFileSync("sybils.txt", sybilsResult);
-
-console.log(`sybils: ${Object.keys(found).length}`);
+console.log(`sybils: ${sybilsCount}`);
